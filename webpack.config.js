c10132071e17a86ddf6658ae289d00582c2873a2
winnnntttter <1013222027@qq.com>
2019-03-05 14:19:21 +0800

    再次启动

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ var APP_PATH = path.resolve(ROOT_PATH, "app");
 var BUILD_PATH = path.resolve(ROOT_PATH, "build");
 
 module.exports = {
-  entry: APP_PATH,
-  output: {
+  entry: APP_PATH,//项目的文件夹 可以直接用文件夹名称 默认会找index.js 也可以确定是哪个文件名字
+  output: {//输出的文件名 合并以后的js会命名为bundle.js
     path: BUILD_PATH,
     filename: "bundle.js"
   },
